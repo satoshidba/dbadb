90af4e4f27bb9358a9f5c252f092b4e60605d838
Ryan DBA <ryan@satoshidba.com>
2024-10-16 02:45:46 +0100

    Adding DACPAC

--- DatabaseObjects/tables/tblInit.sql
+++ DatabaseObjects/tables/tblInit.sql
@@ -1,13 +1,7 @@
--- Create a new table called 'TableName' in schema 'SchemaName'
--- Drop the table if it already exists
-IF OBJECT_ID('dbo.tblInit', 'U') IS NOT NULL
-DROP TABLE dbo.tblInit
-GO
 -- Create the table in the specified schema
 CREATE TABLE dbo.tblInit
 (
     tblInitId INT NOT NULL PRIMARY KEY, -- primary key column
-    Sample1 [NVARCHAR](50) NOT NULL,
-    Sample2 [NVARCHAR](50) NOT NULL
+    Sample1 NVARCHAR(50) NOT NULL,
+    Sample2 NVARCHAR(50) NOT NULL
 );
-GO
